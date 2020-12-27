d581ee2c1406b9f682ea94694a0fbff6ff7db6a8
Lucas Uyezu <lucas.uyezu@shopify.com>
2020-12-27 12:12:16 -0500

    One thread per day

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
+use std::thread;
+
 mod year2020;
 
 fn main() {
-    let year2020_day2_lines = year2020::get_lines("/Users/lucasuyezushopify/src/github.com/lucasuyezu/aoc/src/year2020/day2/input");
+    let t2020_02 = thread::spawn(|| {
+        let year2020_day2_lines = year2020::get_lines("/Users/lucasuyezushopify/src/github.com/lucasuyezu/aoc/src/year2020/day2/input");
 
-    let result = year2020::day2::solve_part_2(
-        &year2020_day2_lines
-    );
-    println!("Year 2020 Day 2 Part 2 result is {}", result);
+        let result = year2020::day2::solve_part_2(
+            &year2020_day2_lines
+        );
+        println!("Year 2020 Day 2 Part 2 result is {}", result);
 
-    let result = year2020::day2::solve_part_1(
-        &year2020_day2_lines
-    );
-    println!("Year 2020 Day 2 Part 1 result is {}", result);
+        let result = year2020::day2::solve_part_1(
+            &year2020_day2_lines
+        );
+        println!("Year 2020 Day 2 Part 1 result is {}", result);
+    });
 
-    let year2020_day1_lines = year2020::get_lines("/Users/lucasuyezushopify/src/github.com/lucasuyezu/aoc/src/year2020/day1/input");
+    let t2020_01 = thread::spawn(|| {
+        let year2020_day1_lines = year2020::get_lines("/Users/lucasuyezushopify/src/github.com/lucasuyezu/aoc/src/year2020/day1/input");
 
-    let result = year2020::day1::solve_part_2(
-        &year2020_day1_lines
-    );
-    println!("Year 2020 Day 1 Part 2 result is {}", result);
+        let result = year2020::day1::solve_part_2(
+            &year2020_day1_lines
+        );
+        println!("Year 2020 Day 1 Part 2 result is {}", result);
 
-    let result = year2020::day1::solve_part_1(
-        &year2020_day1_lines
-    );
-    println!("Year 2020 Day 1 Part 1 result is {}", result);
+        let result = year2020::day1::solve_part_1(
+            &year2020_day1_lines
+        );
+        println!("Year 2020 Day 1 Part 1 result is {}", result);
+    });
+
+    t2020_02.join().expect("Year 2020 Day 2 thread has panicked!");
+    t2020_01.join().expect("Year 2020 Day 1 thread has panicked!");
 }
