bf038b0908cfceb555203260a06478a680a47afa
Lucas Carvalho Uyezu <lucas@uyezu.com>
2022-12-15 16:08:10 -0500

    Year 2022, Day 15, Parts 1 & 2

--- src/year2022/day15/mod.rs
+++ src/year2022/day15/mod.rs
@@ -1,7 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq, Copy)]
-struct Point {
+pub struct Point {
     x: isize,
     y: isize,
 }
@@ -17,21 +17,19 @@ struct Pair {
 struct Grid {
     pairs: Vec<Pair>,
     beacons: HashSet<Point>,
-    sensors: HashSet<Point>,
     min_x: isize,
     max_x: isize,
-    target_y: isize,
 }
 
 fn dist(point_a: &Point, point_b: &Point) -> usize {
     (point_a.x - point_b.x).abs() as usize + (point_a.y - point_b.y).abs() as usize
 }
+
 impl Grid {
-    fn from(input: &str, target_y: isize) -> Grid {
+    fn from(input: &str) -> Grid {
         let mut min_x = isize::MAX;
         let mut max_x = isize::MIN;
 
-        let mut sensors: HashSet<Point> = HashSet::new();
         let mut beacons: HashSet<Point> = HashSet::new();
 
         let pairs: Vec<Pair> = input
@@ -57,7 +55,6 @@ impl Grid {
 
                 let distance = dist(&sensor, &beacon);
 
-                sensors.insert(sensor);
                 beacons.insert(beacon);
 
                 Pair {
@@ -90,15 +87,13 @@ impl Grid {
             pairs,
             min_x,
             max_x,
-            target_y,
             beacons,
-            sensors,
         }
     }
 
-    fn row_count(&self) -> usize {
+    fn row_count(&self, target_y: isize) -> usize {
         let mut point = Point {
-            y: self.target_y,
+            y: target_y,
             x: self.min_x,
         };
         let mut count = 0;
@@ -122,43 +117,121 @@ impl Grid {
 
         count
     }
+
+    fn distress_beacon(&self, point_a: &Point, point_b: &Point) -> Option<Point> {
+        let mut x = point_a.x;
+        while x <= point_b.x {
+            let mut y = point_a.y;
+            while y <= point_b.y {
+                let point = Point { x, y };
+
+                // If I'm a beacon, skip me
+                if self.beacons.contains(&point) {
+                    y += 1;
+                    if y > point_b.y {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+
+                // If I'm within range of *any* sensor-beacon pair, count me
+                if let Some(pair) = self
+                    .pairs
+                    .iter()
+                    .find(|pair| dist(&point, &pair.sensor) <= pair.distance)
+                {
+                    let point_to_sensor = dist(&point, &pair.sensor);
+                    let skip_steps: usize = pair.distance - point_to_sensor;
+
+                    y += 1 + skip_steps as isize;
+                    if y > point_b.y {
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    return Some(point);
+                }
+            }
+            x += 1;
+        }
+
+        None
+    }
 }
 
-pub fn solve_part_1(input: &str, target_y: isize) -> usize {
-    let mut grid = Grid::from(input, target_y);
-
-    dbg!(&grid.min_x);
-    dbg!(&grid.max_x);
-    // grid.populate();
-    grid.row_count()
+pub fn solve_part_1(input: &str) -> usize {
+    let grid = Grid::from(input);
+    grid.row_count(2000000)
 }
 
-pub fn solve_part_2(_input: &str) -> usize {
-    0
+pub fn solve_part_2(input: &str) -> usize {
+    let grid = Grid::from(input);
+
+    let point_a = Point { x: 0, y: 0 };
+    let point_b = Point {
+        x: 4000000,
+        y: 4000000
+    };
+
+    match grid.distress_beacon(&point_a, &point_b) {
+        Some(beacon_point) => beacon_point.x as usize * 4000000 + beacon_point.y as usize,
+        None => panic!("Did not find beacon"),
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::year2022::day15::{Grid, Point};
+
     #[test]
     fn part1_test_input() {
-        assert_eq!(super::solve_part_1(&include_str!("test_input"), 10), 26);
+        let grid = Grid::from(&include_str!("test_input"));
+        assert_eq!(
+            grid.row_count(10),
+            26
+        );
     }
 
     #[test]
     fn part1_real_input() {
+        let grid = Grid::from(&include_str!("input"));
         assert_eq!(
-            super::solve_part_1(&include_str!("input"), 2000000),
+            grid.row_count(2000000),
             4665948
         );
     }
 
     #[test]
     fn part2_test_input() {
-        assert_eq!(super::solve_part_2(&include_str!("test_input")), 140);
+        let point_a = Point { x: 0, y: 0 };
+        let point_b = Point {
+            x: 20,
+            y: 20,
+        };
+        let grid = Grid::from(&include_str!("test_input"));
+        let beacon_point: Point = grid.distress_beacon(&point_a, &point_b);
+
+        assert_eq!(
+            beacon_point.x as usize * 4000000 + beacon_point.y as usize,
+            56000011,
+        );
     }
 
     #[test]
     fn part2_real_input() {
-        assert_eq!(super::solve_part_2(&include_str!("input")), 22184);
+        let point_a = Point { x: 0, y: 0 };
+        let point_b = Point {
+            x: 4000000,
+            y: 4000000
+        };
+        let grid = Grid::from(&include_str!("input"));
+        let beacon_point: Point = grid.distress_beacon(&point_a, &point_b);
+
+        assert_eq!(
+            beacon_point.x as usize * 4000000 + beacon_point.y as usize,
+            13543690671045,
+        );
     }
 }
