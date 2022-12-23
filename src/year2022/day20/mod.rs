c092a9cef64e27972b501d06b1c7299e7e24db78
Lucas Carvalho Uyezu <lucas@uyezu.com>
2022-12-23 10:06:47 -0500

    Year 2022, Day 20, Part 2

--- src/year2022/day20/mod.rs
+++ src/year2022/day20/mod.rs
@@ -1,33 +1,20 @@
-pub fn solve_part_1(input: &str) -> isize {
+fn solve(input: &str, decryption_key: isize, num_rounds: u8) -> isize {
     let mut numbers: Vec<(usize, isize)> = input
         .lines()
         .enumerate()
-        .map(|(i, line)| (i, line.parse::<isize>().unwrap()))
+        .map(|(i, line)| (i, decryption_key * line.parse::<isize>().unwrap()))
         .collect();
 
-    for i in 0..numbers.len() {
-        // find index of i
-        let mut index = numbers.iter().position(|(j, _)| i == *j).unwrap();
+    for _ in 0..num_rounds {
+        for i in 0..numbers.len() {
+            let index = numbers.iter().position(|(j, _)| i == *j).unwrap();
+            let tuple = numbers[index];
 
-        let tuple = numbers[index];
-
-        for _ in 0..tuple.1.abs() {
-            let new_index = if tuple.1 > 0 {
-                if index == numbers.len() - 1 {
-                    1
-                } else {
-                    index + 1
-                }
-            } else if index == 1 {
-                numbers.len() - 1
-            } else if index == 0 {
-                numbers.len() - 2
-            } else {
-                index - 1
-            };
             numbers.remove(index);
+            let new_index: usize =
+                (tuple.1 + index as isize).rem_euclid(numbers.len() as isize) as usize;
+
             numbers.insert(new_index, tuple);
-            index = new_index;
         }
     }
 
@@ -38,19 +25,34 @@ pub fn solve_part_1(input: &str) -> isize {
         + numbers[(zero_index + 3000) % numbers.len()].1
 }
 
-pub fn solve_part_2(_input: &str) -> isize {
-    0
+pub fn solve_part_1(input: &str) -> isize {
+    solve(input, 1, 1)
+}
+pub fn solve_part_2(input: &str) -> isize {
+    solve(input, 811589153, 10)
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn part1_test_input() {
-        assert_eq!(super::solve_part_1(&include_str!("test_input")), 3);
+        assert_eq!(solve_part_1(&include_str!("test_input")), 3);
     }
 
     #[test]
     fn part1_real_input() {
-        assert_eq!(super::solve_part_1(&include_str!("input")), 16533);
+        assert_eq!(solve_part_1(&include_str!("input")), 16533);
+    }
+
+    #[test]
+    fn part2_test_input() {
+        assert_eq!(solve_part_2(&include_str!("test_input")), 1623178306);
+    }
+
+    #[test]
+    fn part2_real_input() {
+        assert_eq!(solve_part_2(&include_str!("input")), 4789999181006);
     }
 }
