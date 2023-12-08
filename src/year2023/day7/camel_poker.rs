5bd076d10b9c1bee40db4e2f39477c483b54d6ca
Lucas Carvalho Uyezu <lucas@uyezu.com>
2023-12-08 17:10:42 -0500

    Year 2023, Day 8, Parts 2: Memoize HandType

--- src/year2023/day7/camel_poker.rs
+++ src/year2023/day7/camel_poker.rs
@@ -7,64 +7,60 @@ use crate::utils::ParseInputError;
 #[derive(Debug, Eq)]
 pub struct Hand {
     pub cards: Vec<char>,
+    pub hand_type: HandType,
     pub bid: usize,
 }
 
-impl Hand {
-    fn card_groupings(&self) -> HashMap<char, usize> {
-        let mut card_groupings: HashMap<char, usize> = HashMap::new();
+fn card_groupings(cards: &Vec<char>) -> HashMap<char, usize> {
+    let mut card_groupings: HashMap<char, usize> = HashMap::new();
 
-        for card in self.cards.iter() {
-            card_groupings.entry(*card).and_modify(|count| *count += 1).or_insert(1);
-        }
-
-        card_groupings
+    for card in cards.iter() {
+        card_groupings.entry(*card).and_modify(|count| *count += 1).or_insert(1);
     }
 
-    fn hand_type(&self) -> HandType {
-        let card_groupings = self.card_groupings();
+    card_groupings
+}
 
-        let card_groupings_without_jokers: HashMap<char, usize> = self
-            .card_groupings()
-            .into_iter()
-            .filter(|(rank, _)| *rank != '0')
-            .collect();
+fn hand_type(cards: &Vec<char>) -> HandType {
+    let card_groupings = card_groupings(cards);
 
-        let mut hands: Vec<&usize> = card_groupings.values().collect();
-        hands.sort();
+    let card_groupings_without_jokers: HashMap<&char, &usize> =
+        card_groupings.iter().filter(|(rank, _)| **rank != '0').collect();
 
-        let hands_without_jokers = card_groupings_without_jokers.values().collect::<Vec<&usize>>();
+    let mut hands: Vec<&usize> = card_groupings.values().collect();
+    hands.sort();
 
-        let max_hand_without_jokers: usize = *card_groupings_without_jokers.values().max().unwrap_or(&&0);
+    let hands_without_jokers = card_groupings_without_jokers.values().collect::<Vec<&&usize>>();
 
-        let jokers = *card_groupings.get(&'0').unwrap_or(&0);
+    let max_hand_without_jokers: usize = **card_groupings_without_jokers.values().max().unwrap_or(&&0);
 
-        if max_hand_without_jokers + jokers == 5 {
-            return HandType::FiveOfAKind;
-        }
+    let jokers = *card_groupings.get(&'0').unwrap_or(&0);
 
-        if max_hand_without_jokers + jokers == 4 {
-            return HandType::FourOfAKind;
-        }
-
-        if (hands == vec![&2, &3]) || (hands_without_jokers == vec![&2, &2] && jokers == 1) {
-            return HandType::FullHouse;
-        }
-
-        if max_hand_without_jokers + jokers == 3 {
-            return HandType::ThreeOfAKind;
-        }
-
-        if hands == vec![&1, &2, &2] {
-            return HandType::TwoPair;
-        }
-
-        if max_hand_without_jokers + jokers == 2 {
-            return HandType::OnePair;
-        }
-
-        HandType::HighCard
+    if max_hand_without_jokers + jokers == 5 {
+        return HandType::FiveOfAKind;
     }
+
+    if max_hand_without_jokers + jokers == 4 {
+        return HandType::FourOfAKind;
+    }
+
+    if (hands == vec![&2, &3]) || (hands_without_jokers == vec![&&2, &&2] && jokers == 1) {
+        return HandType::FullHouse;
+    }
+
+    if max_hand_without_jokers + jokers == 3 {
+        return HandType::ThreeOfAKind;
+    }
+
+    if hands == vec![&1, &2, &2] {
+        return HandType::TwoPair;
+    }
+
+    if max_hand_without_jokers + jokers == 2 {
+        return HandType::OnePair;
+    }
+
+    HandType::HighCard
 }
 
 impl FromStr for Hand {
@@ -72,16 +68,16 @@ impl FromStr for Hand {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (cards_str, bid_str) = s.split_once(" ").unwrap();
+        let cards = cards_str.chars().collect();
+        let bid = bid_str.parse().unwrap();
+        let hand_type = hand_type(&cards);
 
-        Ok(Hand {
-            cards: cards_str.chars().collect(),
-            bid: bid_str.parse().unwrap(),
-        })
+        Ok(Hand { cards, bid, hand_type })
     }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum HandType {
+pub enum HandType {
     FiveOfAKind = 7,
     FourOfAKind = 6,
     FullHouse = 5,
@@ -105,8 +101,6 @@ impl PartialOrd for Hand {
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.hand_type()
-            .cmp(&other.hand_type())
-            .then(self.cards.cmp(&other.cards))
+        self.hand_type.cmp(&other.hand_type).then(self.cards.cmp(&other.cards))
     }
 }
