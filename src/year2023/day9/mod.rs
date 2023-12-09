2415a69fab8c0ba3430d9e0e2d4407fef06db0be
Lucas Carvalho Uyezu <lucas@uyezu.com>
2023-12-09 01:36:08 -0500

    Year 2023, Day 9, Parts 1 & 2

---
+++ src/year2023/day9/mod.rs
@@ -0,0 +1,75 @@
+fn parse_input(input: &str) -> Vec<Vec<isize>> {
+    input
+        .lines()
+        .map(|line| line.split(" ").map(|n| n.parse().unwrap()).collect())
+        .collect()
+}
+
+fn calculate_sequences(history: Vec<isize>) -> Vec<Vec<isize>> {
+    let mut levels: Vec<Vec<isize>> = vec![];
+    levels.push(history.clone());
+
+    let mut last_level = levels.last().unwrap().clone();
+
+    while !last_level.iter().all(|level| *level == 0) {
+        let mut current_level: Vec<isize> = vec![];
+
+        for i in 1..last_level.len() {
+            current_level.push(last_level.get(i).unwrap() - last_level.get(i - 1).unwrap());
+        }
+
+        levels.push(current_level.clone());
+        last_level = current_level;
+    }
+
+    levels
+}
+
+pub fn solve_part_1(input: &str) -> isize {
+    parse_input(input)
+        .into_iter()
+        .map(calculate_sequences)
+        .map(|levels| levels.iter().map(|level| *level.last().unwrap()).sum::<isize>())
+        .sum()
+}
+
+pub fn solve_part_2(input: &str) -> isize {
+    parse_input(input)
+        .into_iter()
+        .map(calculate_sequences)
+        .map(|mut levels| {
+            levels.reverse();
+
+            for i in 1..levels.len() {
+                let x = levels.get(i - 1).unwrap().first().unwrap().clone();
+                let level = levels.get_mut(i).unwrap();
+                level.insert(0, level.first().unwrap() - x);
+            }
+
+            *levels.last().unwrap().first().unwrap()
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1_test_input() {
+        assert_eq!(super::solve_part_1(&include_str!("test_input")), 114);
+    }
+
+    #[test]
+    fn part1_real_input() {
+        assert_eq!(super::solve_part_1(&include_str!("input")), 1955513104);
+    }
+
+    #[test]
+    fn part2_test_input() {
+        assert_eq!(super::solve_part_2(&include_str!("test_input")), 2);
+    }
+
+    #[test]
+    fn part2_real_input() {
+        assert_eq!(super::solve_part_2(&include_str!("input")), 1_131);
+    }
+}
