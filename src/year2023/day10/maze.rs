0d90766aec336ccd47c9b628134b5c15ca061c9b
Lucas Carvalho Uyezu <lucas@uyezu.com>
2023-12-11 17:43:20 -0500

    Year 2023, Day 10: Part 2

--- src/year2023/day10/maze.rs
+++ src/year2023/day10/maze.rs
@@ -8,156 +8,130 @@ use crate::utils::{
 #[derive(Debug)]
 pub struct Maze {
     data: Vec<Vec<char>>,
-    pub starting_point: Point,
+    start: Point,
+    x_len: usize,
+    y_len: usize,
 }
 
 impl FromStr for Maze {
     type Err = ParseInputError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut maze = Maze {
-            data: s.lines().map(|line| line.chars().collect()).collect(),
-            starting_point: Point { x: 0, y: 0 },
+        let (start_str, rest) = s.split_once("\n").unwrap();
+        let (start_x_str, start_y_str) = start_str.split_once(",").unwrap();
+        let start = Point {
+            x: start_x_str.parse().unwrap(),
+            y: start_y_str.parse().unwrap(),
         };
+        let data: Vec<Vec<char>> = rest.lines().map(|line| line.chars().collect()).collect();
+        let x_len = data.len();
+        let y_len = data.get(0).unwrap().len();
 
-        maze.starting_point = start_point(&maze);
-
-        Ok(maze)
+        Ok(Maze {
+            data,
+            start,
+            x_len,
+            y_len,
+        })
     }
-}
-
-fn start_point(maze: &Maze) -> Point {
-    for (x, line) in maze.data.iter().enumerate() {
-        for (y, c) in line.iter().enumerate() {
-            if *c == 'S' {
-                return Point {
-                    x: (x as isize),
-                    y: (y as isize),
-                };
-            }
-        }
-    }
-
-    panic!("Could not find the starting point")
 }
 
 impl Maze {
-    pub fn walk(&self) -> usize {
-        println!("Starting at {:?}", self.starting_point);
-
+    pub fn walk(&self) -> HashSet<Point> {
         let mut visited_points: HashSet<Point> = HashSet::new();
-        visited_points.insert(self.starting_point);
+        visited_points.insert(self.start);
 
-        let mut n = 1;
-        let mut current_point = self.starting_neighbour(self.starting_point);
-        while current_point != self.starting_point {
-            let c = self.get_char(current_point).unwrap();
-            println!("Now on {} at {:?}", c, current_point);
+        let mut current_point = *self.neighbours(self.start).first().unwrap();
+        while current_point != self.start {
             visited_points.insert(current_point);
+
             let neighbours = self.neighbours(current_point);
-            println!("\tNeighbours are {:?}", neighbours);
-
-            assert!(neighbours.len() <= 2);
-
             current_point = *neighbours
                 .iter()
                 .find(|neighbour| !visited_points.contains(neighbour))
-                .unwrap_or(&self.starting_point);
-
-            n += 1;
+                .unwrap_or(&self.start);
         }
 
-        println!("Came back to starting pos after {:?} nodes", n);
-
-        n / 2
+        visited_points
     }
 
-    fn starting_neighbour(&self, point: Point) -> Point {
-        if self.connecting_points(point + NORTH).contains(&point) {
-            point + NORTH
-        } else if self.connecting_points(point + EAST).contains(&point) {
-            point + EAST
-        } else if self.connecting_points(point + SOUTH).contains(&point) {
-            point + SOUTH
-        } else if self.connecting_points(point + WEST).contains(&point) {
-            point + WEST
-        } else {
-            panic!("Could not find starting neighbour");
+    pub fn enclosed_points_count(&self, maze_points: HashSet<Point>) -> usize {
+        let mut result = 0;
+        for x in 0..self.x_len {
+            for y in 0..self.y_len {
+                let current_point = Point {
+                    x: (x as isize),
+                    y: (y as isize),
+                };
+
+                if maze_points.contains(&current_point) {
+                    continue;
+                }
+
+                let mut enclosed = false;
+                let mut current_line = '.';
+                let mut xx_point = current_point;
+                let direction = NORTH;
+                for _ in (0..x).rev() {
+                    xx_point = xx_point + direction;
+                    let xx_char = self.get_char(xx_point);
+
+                    let is_maze_point = maze_points.contains(&xx_point);
+                    if !is_maze_point {
+                        continue;
+                    }
+
+                    if xx_char == 'J' || xx_char == 'L' {
+                        current_line = xx_char;
+                    } else if xx_char == '7' || xx_char == 'F' {
+                        if (current_line == 'J' && xx_char == 'F') || (current_line == 'L' && xx_char == '7') {
+                            enclosed = !enclosed;
+                        }
+                        current_line = '.'
+                    } else if xx_char == '-' {
+                        enclosed = !enclosed;
+                    }
+                }
+
+                if enclosed {
+                    result += 1;
+                }
+            }
         }
+
+        result
     }
 
     fn neighbours(&self, point: Point) -> Vec<Point> {
         let mut neighbours: Vec<Point> = vec![];
 
         let my_connections = self.connecting_points(point);
-        dbg!(&my_connections);
 
         let north = point + NORTH;
         if my_connections.contains(&north) && self.connecting_points(north).contains(&point) {
             neighbours.push(point + NORTH);
         }
-        dbg!(&neighbours);
 
         let east = point + EAST;
         if my_connections.contains(&east) && self.connecting_points(east).contains(&point) {
             neighbours.push(point + EAST);
         }
-        dbg!(&neighbours);
 
         let south = point + SOUTH;
         if my_connections.contains(&south) && self.connecting_points(south).contains(&point) {
             neighbours.push(point + SOUTH);
         }
-        dbg!(&neighbours);
 
         let west = point + WEST;
         if my_connections.contains(&west) && self.connecting_points(west).contains(&point) {
             neighbours.push(point + WEST);
         }
-        dbg!(&neighbours);
 
         neighbours
     }
 
-    // fn neighbours(&self, point: Point) -> Vec<Point> {
-    //     let mine: HashSet<Point> = HashSet::from_iter(self.connects_to(point));
-    //     dbg!(&mine);
-
-    //     let mut theirs: Vec<Point> = vec![];
-    //     theirs.append(&mut self.connects_to(point + NORTH));
-    //     dbg!(&theirs);
-    //     todo!();
-    //     theirs.append(&mut self.connects_to(point + EAST));
-    //     dbg!(&theirs);
-    //     theirs.append(&mut self.connects_to(point + SOUTH));
-    //     dbg!(&theirs);
-    //     theirs.append(&mut self.connects_to(point + WEST));
-    //     dbg!(&theirs);
-
-    //     dbg!(&theirs);
-    //     mine.intersection(&HashSet::from_iter(theirs)).map(|p| *p).collect()
-    // }
-
-    // pub fn pipe_type(&self, point: Point) -> Result<char, &'static str> {
-    //     let east = self.get_char(point + EAST);
-    //     let west = self.get_char(point + WEST);
-    //     let south = self.get_char(point + SOUTH);
-    //     let north = self.get_char(point + NORTH);
-    //     println!("Point to the east is {:?}", east);
-    //     println!("Point to the south is {:?}", south);
-    //     println!("Point to the west is {:?}", west);
-
-    //     if self.connects_to(point, EAST) && self.connects_to(point, SOUTH) {
-    //         Ok('F')
-    //     } else {
-    //         Err("Could not find pipe type")
-    //     }
-    // }
-
     fn connecting_points(&self, point: Point) -> Vec<Point> {
-        let c = self.get_char(point).unwrap_or('x');
-        println!("\tPoints connecting to {} at {:?}", c, point);
-        match c {
+        match self.get_char(point) {
             '|' => vec![point + NORTH, point + SOUTH],
             '-' => vec![point + EAST, point + WEST],
             'L' => vec![point + NORTH, point + EAST],
@@ -168,16 +142,7 @@ impl Maze {
         }
     }
 
-    fn has_neighbour(&self, point: Point) -> bool {
-        (0..self.data.len()).contains(&(point.x as usize))
-            && (0..self.data.get(point.x as usize).unwrap().len()).contains(&(point.y as usize))
-    }
-
-    pub fn get_char(&self, point: Point) -> Option<char> {
-        if self.has_neighbour(point) {
-            Some(self.data.as_slice()[point.x as usize].as_slice()[point.y as usize])
-        } else {
-            None
-        }
+    pub fn get_char(&self, point: Point) -> char {
+        self.data[point.x as usize][point.y as usize]
     }
 }
--- src/year2023/day10/mod.rs
+++ src/year2023/day10/mod.rs
@@ -3,11 +3,14 @@ mod maze;
 use crate::year2023::day10::maze::Maze;
 
 pub fn solve_part_1(input: &str) -> usize {
-    input.parse::<Maze>().unwrap().walk()
+    let visited_points = input.parse::<Maze>().unwrap().walk();
+    visited_points.len() / 2
 }
 
-pub fn solve_part_2(_input: &str) -> usize {
-    0
+pub fn solve_part_2(input: &str) -> usize {
+    let maze = input.parse::<Maze>().unwrap();
+    let visited_points = maze.walk();
+    maze.enclosed_points_count(visited_points)
 }
 
 #[cfg(test)]
@@ -23,17 +26,26 @@ mod tests {
     }
     #[test]
     fn part1_real_input() {
-        // 75 is wrong
         assert_eq!(super::solve_part_1(&include_str!("input")), 6754);
     }
 
     #[test]
-    fn part2_test_input() {
-        assert_eq!(super::solve_part_2(&include_str!("test_input_square_loop")), 71_503);
+    fn part2_test_input_small() {
+        assert_eq!(super::solve_part_2(&include_str!("test_input_part_2_small")), 4);
+    }
+
+    #[test]
+    fn part2_test_input_medium() {
+        assert_eq!(super::solve_part_2(&include_str!("test_input_part_2_medium")), 8);
+    }
+
+    #[test]
+    fn part2_test_input_large() {
+        assert_eq!(super::solve_part_2(&include_str!("test_input_part_2_large")), 10);
     }
 
     #[test]
     fn part2_real_input() {
-        assert_eq!(super::solve_part_2(&include_str!("input")), 34_655_848);
+        assert_eq!(super::solve_part_2(&include_str!("input")), 567);
     }
 }
