2d540a8f9b3c05d7471b091e111e13be107defef
Lucas Carvalho Uyezu <lucas@uyezu.com>
2023-12-14 22:14:55 -0500

    Year 2023, Day 14: Part 2

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,9 @@
 pub mod point;
 pub mod snafu;
 
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct ParseInputError;
 
@@ -31,11 +34,89 @@ pub fn lcm(arr: &[usize]) -> usize {
 }
 
 pub fn print_2d_vec(vec: &Vec<Vec<char>>) {
+    let mut output = String::with_capacity(vec.len() * vec[0].len() + vec.len());
     for x in 0..vec.len() {
         for y in 0..vec[x].len() {
-            print!("{}", vec[x][y]);
+            output.push(vec[x][y]);
         }
-        println!();
+        output.push('\n');
     }
-    println!();
+    println!("{}", output);
+}
+
+pub fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+// a,b,c (0,0), (0,1), (0,2)
+// d,e,f (1,0), (1,1), (1,2)
+//
+// becomes
+//
+// d,a (0,0), (0,1)
+// e,b (1,0), (1,1)
+// c,f (2,0), (2,1)
+pub fn rotate(v: Vec<Vec<char>>) -> Option<Vec<Vec<char>>> {
+    let x_len = v.len();
+    let y_len = v[0].len();
+
+    let mut result = vec![];
+
+    for new_y in 0..y_len {
+        let mut new_row: Vec<char> = vec![];
+        for new_x in (0..x_len).rev() {
+            new_row.push(v[new_x][new_y].clone());
+        }
+        result.push(new_row);
+    }
+
+    Some(result)
+}
+
+// a,b,c (0,0), (0,1), (0,2)
+// d,e,f (1,0), (1,1), (1,2)
+//
+// becomes
+//
+// c,f (0,0), (0,1)
+// b,e (1,0), (1,1)
+// a,d (2,0), (2,1)
+pub fn rotate_left(v: Vec<Vec<char>>) -> Option<Vec<Vec<char>>> {
+    let x_len = v.len();
+    let y_len = v[0].len();
+
+    let mut result = vec![];
+
+    for new_y in (0..y_len).rev() {
+        let mut new_row: Vec<char> = vec![];
+        for new_x in 0..x_len {
+            new_row.push(v[new_x][new_y].clone());
+        }
+        result.push(new_row);
+    }
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::rotate;
+    use super::rotate_left;
+
+    #[test]
+    fn test_rotate() {
+        let vec = vec![vec!['a', 'b', 'c'], vec!['d', 'e', 'f']];
+        assert_eq!(rotate(vec), Some(vec![vec!['d', 'a'], vec!['e', 'b'], vec!['f', 'c']]));
+    }
+
+    #[test]
+    fn test_rotate_left() {
+        let vec = vec![vec!['a', 'b', 'c'], vec!['d', 'e', 'f']];
+        assert_eq!(
+            rotate_left(vec),
+            Some(vec![vec!['c', 'f'], vec!['b', 'e'], vec!['a', 'd']])
+        );
+    }
 }
--- src/year2023/day14.rs
+++ src/year2023/day14.rs
@@ -1,11 +1,12 @@
+use crate::utils::calculate_hash;
 use itertools::Itertools;
-
-use crate::utils::print_2d_vec;
+use std::collections::HashMap;
 
 type Grid = Vec<Vec<char>>;
 
 const ROUNDED_ROCK: char = 'O';
 const EMPTY_SPACE: char = '.';
+const CYCLES: usize = 1_000_000_000;
 
 fn parse_input(input: &str) -> Option<Grid> {
     Some(
@@ -16,19 +17,14 @@ fn parse_input(input: &str) -> Option<Grid> {
     )
 }
 
-fn tilt(mut grid: Grid) -> Option<Grid> {
-    print_2d_vec(&grid);
-
+fn tilt_north(mut grid: Grid) -> Option<Grid> {
     let x_len = grid.len();
     let y_len = grid[0].len();
 
-    // for each column
     for y in 0..y_len {
         for x in 0..x_len {
-            // for each ROUNDED_ROCK
             if grid[x][y] == ROUNDED_ROCK {
                 let mut xx = x;
-                // Keep bumping it up until you reach the top OR anything other than an EMPTY_SPACE
                 while xx > 0 && grid[xx - 1][y] == EMPTY_SPACE {
                     grid[xx - 1][y] = ROUNDED_ROCK;
                     grid[xx][y] = EMPTY_SPACE;
@@ -41,27 +37,117 @@ fn tilt(mut grid: Grid) -> Option<Grid> {
     Some(grid)
 }
 
-fn calculate_total_load(grid: Grid) -> Option<usize> {
-    print_2d_vec(&grid);
+fn tilt_west(mut grid: Grid) -> Option<Grid> {
     let x_len = grid.len();
+    let y_len = grid[0].len();
 
+    for x in 0..x_len {
+        for y in 0..y_len {
+            if grid[x][y] == ROUNDED_ROCK {
+                let mut yy = y;
+                while yy > 0 && grid[x][yy - 1] == EMPTY_SPACE {
+                    grid[x][yy - 1] = ROUNDED_ROCK;
+                    grid[x][yy] = EMPTY_SPACE;
+                    yy -= 1;
+                }
+            }
+        }
+    }
+
+    Some(grid)
+}
+
+fn tilt_south(mut grid: Grid) -> Option<Grid> {
+    let x_len = grid.len();
+    let y_len = grid[0].len();
+
+    for y in 0..y_len {
+        for x in (0..x_len).rev() {
+            if grid[x][y] == ROUNDED_ROCK {
+                let mut xx = x;
+                while xx < x_len - 1 && grid[xx + 1][y] == EMPTY_SPACE {
+                    grid[xx + 1][y] = ROUNDED_ROCK;
+                    grid[xx][y] = EMPTY_SPACE;
+                    xx += 1;
+                }
+            }
+        }
+    }
+
+    Some(grid)
+}
+
+fn tilt_east(mut grid: Grid) -> Option<Grid> {
+    let x_len = grid.len();
+    let y_len = grid[0].len();
+
+    for x in 0..x_len {
+        for y in (0..y_len).rev() {
+            if grid[x][y] == ROUNDED_ROCK {
+                let mut yy = y;
+                while yy < y_len - 1 && grid[x][yy + 1] == EMPTY_SPACE {
+                    grid[x][yy + 1] = ROUNDED_ROCK;
+                    grid[x][yy] = EMPTY_SPACE;
+                    yy += 1;
+                }
+            }
+        }
+    }
+
+    Some(grid)
+}
+
+fn cycle(grid: Grid) -> Option<Grid> {
+    Some(grid)
+        .and_then(tilt_north)
+        .and_then(tilt_west)
+        .and_then(tilt_south)
+        .and_then(tilt_east)
+}
+
+fn find_cycle(mut grid: Grid) -> Option<Grid> {
+    let iterations = 1_000;
+    let mut idx_hash: HashMap<u64, usize> = HashMap::new();
+    let mut grid_hash: HashMap<usize, Grid> = HashMap::new();
+
+    for i in 0..iterations {
+        let idx_hash_key = calculate_hash(&grid);
+
+        if let Some(cycle_start) = idx_hash.get(&idx_hash_key) {
+            let cycle_size = i - cycle_start;
+            let grid_hash_key = CYCLES - ((CYCLES - cycle_start) / cycle_size) * cycle_size;
+
+            return Some(grid_hash.get(&grid_hash_key).unwrap().clone());
+        } else {
+            grid_hash.insert(i, grid.clone());
+            grid = cycle(grid).unwrap();
+            idx_hash.insert(idx_hash_key, i);
+        }
+    }
+    panic!("Did not find a cycle in {} iterations", iterations);
+}
+
+fn calculate_total_load(grid: Grid) -> Option<usize> {
     Some(
         grid.iter()
             .enumerate()
-            .map(|(i, row)| row.iter().filter(|c| **c == ROUNDED_ROCK).count() * (x_len - i))
+            .map(|(i, row)| row.iter().filter(|c| **c == ROUNDED_ROCK).count() * (grid.len() - i))
             .sum(),
     )
 }
 
 pub fn solve_part_1(input: &str) -> usize {
     parse_input(input)
-        .and_then(tilt)
+        .and_then(tilt_north)
         .and_then(calculate_total_load)
         .unwrap()
 }
 
-pub fn solve_part_2(_input: &str) -> usize {
-    0
+pub fn solve_part_2(input: &str) -> usize {
+    parse_input(input)
+        .and_then(find_cycle)
+        .and_then(calculate_total_load)
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -73,16 +159,16 @@ mod tests {
 
     #[test]
     fn part1_real_input() {
-        assert_eq!(super::solve_part_1(&include_str!("day14/input")), 30_575);
+        assert_eq!(super::solve_part_1(&include_str!("day14/input")), 107_951);
     }
 
     #[test]
     fn part2_test_input() {
-        assert_eq!(super::solve_part_2(&include_str!("day14/test_input")), 400);
+        assert_eq!(super::solve_part_2(&include_str!("day14/test_input")), 64);
     }
 
     #[test]
     fn part2_real_input() {
-        assert_eq!(super::solve_part_2(&include_str!("day14/input")), 37_478);
+        assert_eq!(super::solve_part_2(&include_str!("day14/input")), 95_736);
     }
 }
