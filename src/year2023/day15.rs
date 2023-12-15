45e2a93e5900ce30d2646f98b73d93fdebf0761b
Lucas Carvalho Uyezu <lucas@uyezu.com>
2023-12-15 01:52:07 -0500

    Year 2023, Day 15: Part 2

--- src/year2023/day15.rs
+++ src/year2023/day15.rs
@@ -1,22 +1,57 @@
+type Box = Vec<(String, usize)>;
+
 fn hash(s: &str) -> usize {
-    let mut result = 0;
+    s.chars().fold(0, |mut acc, c| {
+        acc += c as usize;
+        acc *= 17;
+        acc.rem_euclid(256)
+    })
+}
 
-    for c in s.trim().chars() {
-        let current_value = c as usize;
-        result += current_value;
-        result *= 17;
-        result = result.rem_euclid(256);
+fn populate_boxes(s: &str, boxes: &mut Vec<Box>) {
+    let split_char = match s.contains("=") {
+        true => '=',
+        false => '-',
+    };
+
+    let (label, focal_length_str) = s.split_once(split_char).unwrap();
+    let box_id = hash(label);
+
+    if s.contains("=") {
+        let focal_length: usize = focal_length_str.parse().unwrap();
+        if let Some(pos) = boxes[box_id].iter().position(|(lens_label, _)| lens_label == label) {
+            boxes[box_id].remove(pos);
+            boxes[box_id].insert(pos, (label.to_string(), focal_length));
+        } else {
+            boxes[box_id].push((label.to_string(), focal_length));
+        }
+    } else {
+        if let Some(pos) = boxes[box_id].iter().position(|(lens_label, _)| lens_label == label) {
+            boxes[box_id].remove(pos);
+        }
     }
-
-    println!("{} becomes {}", s, result);
-    result
 }
+
+fn focusing_power((box_id, cur_box): (usize, &Box)) -> usize {
+    cur_box
+        .iter()
+        .enumerate()
+        .map(|(slot_id, (_label, focal_length))| (1 + box_id) * (1 + slot_id) * focal_length)
+        .sum::<usize>()
+}
+
 pub fn solve_part_1(input: &str) -> usize {
-    input.split(",").map(hash).sum()
+    input.trim().split(",").map(hash).sum()
 }
 
-pub fn solve_part_2(_: &str) -> usize {
-    1
+pub fn solve_part_2(input: &str) -> usize {
+    let mut boxes: Vec<Box> = vec![vec![]; 256];
+
+    input.trim().split(",").for_each(|s| {
+        populate_boxes(s, &mut boxes);
+    });
+
+    boxes.iter().enumerate().map(focusing_power).sum()
 }
 
 #[cfg(test)]
@@ -40,11 +75,11 @@ mod tests {
 
     #[test]
     fn part2_test_input() {
-        assert_eq!(super::solve_part_2(&include_str!("day15/test_input")), 0);
+        assert_eq!(super::solve_part_2(&include_str!("day15/test_input")), 145);
     }
 
     #[test]
     fn part2_real_input() {
-        assert_eq!(super::solve_part_2(&include_str!("day15/input")), 0);
+        assert_eq!(super::solve_part_2(&include_str!("day15/input")), 215_827);
     }
 }
