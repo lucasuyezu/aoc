22b4d0e3b4c8ef1968b4e621909615fe5c3bc94f
Lucas Carvalho Uyezu <lucas@uyezu.com>
2024-12-09 02:01:56 -0500

    Year 2024, Day 9: Part 2

--- src/year2024/day9/day9.rb
+++ src/year2024/day9/day9.rb
@@ -1,41 +1,41 @@
 #!/usr/bin/env ruby
 
-def solve_part_1 input
-  disk = []
+EmptyBlock = Struct.new(:size, :pos)
+FileBlock = Struct.new(:id, :size, :pos)
 
+def expand_disk(input)
+  disk = []
+  empty_blocks = []
+  file_blocks = []
   input.chars.each_with_index do |char, i|
-    char.to_i.times do
-      disk << if i.even?
-        i / 2
-      else
-        "."
-      end
+    blk_size = char.to_i
+    next if blk_size.zero?
+
+    if i.even?
+      file_blocks << FileBlock.new(i / 2, blk_size, disk.size)
+      blk_size.times { disk << i / 2 }
+    else
+      empty_blocks << EmptyBlock.new(blk_size, disk.size)
+      blk_size.times { disk << "." }
     end
   end
+  [disk, empty_blocks, file_blocks]
+end
 
-  puts disk.inspect
-
-  puts "expanded string has #{disk.size} blocks"
-
+def solve_part_1 input
+  disk, _ = expand_disk(input)
   dot_idx = 0
   blk_idx = disk.size - 1
-  keep_going = true
 
-  while keep_going
+  while dot_idx < blk_idx
     dot_idx += 1 while disk[dot_idx] != "."
     blk_idx -= 1 while disk[blk_idx] == "."
 
     if dot_idx < blk_idx
-      puts "swaping blocks #{dot_idx} and #{blk_idx}"
-
       disk[dot_idx] = disk[blk_idx]
       disk[blk_idx] = "."
-    else
-      keep_going = false
     end
   end
-
-  puts disk.inspect
 
   checksum = 0
 
@@ -49,6 +49,37 @@ def solve_part_1 input
 end
 
 def solve_part_2 input
+  disk, empty_blocks, file_blocks = expand_disk(input)
+
+  file_blocks.reverse.each do |file_block|
+    empty_block_idx = empty_blocks.index { it.size >= file_block.size && it.pos < file_block.pos }
+
+    if empty_block_idx
+      empty_block = empty_blocks[empty_block_idx]
+
+      empty_block_range = empty_block.pos...(empty_block.pos + file_block.size)
+      file_block_range = file_block.pos...(file_block.pos + file_block.size)
+
+      # swap blocks
+      disk[empty_block_range] = disk[file_block_range]
+      disk[file_block_range] = ["."] * file_block.size
+
+      if empty_block.size == file_block.size
+        empty_blocks.delete_at(empty_block_idx)
+      else
+        empty_block.size -= file_block.size
+        empty_block.pos += file_block.size
+      end
+    end
+  end
+
+  checksum = 0
+
+  disk.each_with_index do |blk, i|
+    checksum += i * blk if blk != "."
+  end
+
+  checksum
 end
 
 require 'minitest/autorun'
@@ -63,10 +94,10 @@ class Day1Test < Minitest::Test
   end
 
   def test_part_2_sample_input
-    assert_equal(0, solve_part_2(File.read("test_input")))
+    assert_equal(2858, solve_part_2(File.read("test_input")))
   end
 
   def test_part_2_real_input
-    assert_equal(0, solve_part_2(File.read("input")))
+    assert_equal(6408966547049, solve_part_2(File.read("input")))
   end
 end
